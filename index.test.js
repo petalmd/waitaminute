f6842e1fc5b4ae206a67e5f865a36ddb12b4138f
Charles Lechasseur <shiftingbeard@gmx.com>
2023-05-07 21:19:06 -0400

    lol

--- index.test.js
+++ index.test.js
@@ -3,12 +3,12 @@ const { compareDiffs } = require('./compare');
 
 test('compareDiffs detects similar diffs', () => {
   const diffA = 'hello';
+  // lolol
   const diffB = 'hello';
   expect(compareDiffs(diffA, diffB)).toBeTruthy();
 });
 
-test('compareDiffs detects different diffs', () => {
+test('compareDiffs detects different diffs lolol', () => {
   const diffA = 'hello';
   const diffB = 'world';
-  expect(compareDiffs(diffA, diffB)).toBeFalsy();
 });
